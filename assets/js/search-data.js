30d51450f20efc28e7c660763338508609a58086
mvshivaani <56979873+mvshivaani@users.noreply.github.com>
2026-01-23 06:25:03 +0000

    Deploying to gh-pages from @ mvshivaani/mvshivaani.github.io@0ea58d11498fbf96289a94fdae00f33a594d74bd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -521,13 +521,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/shivaani-mv", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=_w2tafgAAAAJ", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
